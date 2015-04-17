8c2dcf5d29e4bae821363c48f4bd790395800072
Dustin Shaffer <dustin.n.shaffer@gmail.com>
2015-04-17 01:17:26 -0700

    - Added more luaj functionality

---
+++ luasrc/AWTKeycodes.lua
@@ -0,0 +1,9 @@
+local public = {}
+
+public.W = 87
+public.A = 65
+public.S = 83
+public.D = 68
+public.P = 80
+
+return public
--- luasrc/AWTPacDaddyGame.lua
+++ luasrc/AWTPacDaddyGame.lua
@@ -1,6 +1,7 @@
 local Color = luajava.bindClass("java.awt.Color")
 local JFrame = luajava.bindClass("javax.swing.JFrame")
 local Graphics2D = luajava.bindClass("java.awt.Graphics2D")
+local KEYS = require("luasrc/AWTKeycodes")
 
 game = luajava.newInstance("Engine.PacDaddyGame")
 boardReader = game:getBoardReader()
@@ -13,17 +14,28 @@ local colormap = {
     ["FLOOR"]  = Color.BLACK,
 }
 
+local function input(command)
+    inputProcessor:sendCommand(command)
+end
+
+local function getAttribute(attribute)
+    return attributeReader:getValueOf(attribute)
+end
+
+local function conditionalInput(condition, commandT, commandF)
+    if condition then
+        input(commandT)
+    else
+        input(commandF)
+    end
+end
+
 local inputProcesses = {
-    ['w'] = function() inputProcessor:sendCommand("UP")    end,
-    ['a'] = function() inputProcessor:sendCommand("LEFT")  end,
-    ['s'] = function() inputProcessor:sendCommand("DOWN")  end,
-    ['d'] = function() inputProcessor:sendCommand("RIGHT") end,
-    ['p'] = function() if attributeReader:getValueOf("IS_PAUSED") then 
-                           inputProcessor:sendCommand("PLAY") 
-                       else 
-                           inputProcessor:sendCommand("PAUSE") 
-                       end 
-    end, 
+    [KEYS.W] = function() input("UP")    end,
+    [KEYS.A] = function() input("LEFT")  end,
+    [KEYS.S] = function() input("DOWN")  end,
+    [KEYS.D] = function() input("RIGHT") end,
+    [KEYS.P] = function() conditionalInput(getAttribute("IS_PAUSED"), "PLAY", "PAUSE") end, 
 }
 
 local function getTileColor(tilename)
@@ -48,8 +60,8 @@ end
 local function makeKeyInputProcessor()
     local keylistener = luajava.createProxy("java.awt.event.KeyListener", {
         keyPressed = function(e)
-            local keychar = e:getKeyChar()
-            local inputProcess = inputProcesses[keychar]
+            local keycode = e:getKeyCode()
+            local inputProcess = inputProcesses[keycode]
             if type(inputProcess) == "function" then
                 inputProcess()
             end
@@ -68,21 +80,22 @@ local function makeGameDrawer(panel)
     
     local drawfunction = luajava.createProxy("functionpointers.VoidFunctionPointer", {
         call = function()
-            --local board = boardReader:getTiledBoard()
-            --local TILEWIDTH = panel:getWidth() / board[0].length
-            --local TILEHEIGHT = panel:getHeight() / board.length
+        
+            local tilenames = boardReader:getTileNames()
+            local board = boardReader:getTiledBoard()
+            local TILEWIDTH = panel:getWidth() / board[1].length
+            local TILEHEIGHT = panel:getHeight() / board.length
             local g = panel:getGraphics()
             
-            g:setColor(Color.BLUE)
-            g:fillRect(0,0,panel:getWidth(), panel:getHeight())
-            
---            for row = 0, board.length do
---                for col = 0, board[0].length do
---                    local color = getTileColor(board[row][col])
---                    g:setColor(color)
---                    g:fillRect(col * TILEWIDTH, row * TILEHEIGHT, TILEWIDTH, TILEHEIGHT)
---                end
---            end
+            for row = 1, board.length do
+                for col = 1, board[1].length do
+                    local tileEnum = board[row][col]
+                    local tileName = tilenames[tileEnum+1]
+                    local tileColor = getTileColor(tileName)
+                    g:setColor(tileColor)
+                    g:fillRect((col-1) * TILEWIDTH, (row-1) * TILEHEIGHT, TILEWIDTH, TILEHEIGHT)
+                end
+            end
         end
     })
     drawer:addFunction(drawfunction)
