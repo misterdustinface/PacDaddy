d4c528b06e87a658843a79c55d4d0ee6c0c0d210
dustin <misterdustinface@gmail.com>
2015-04-27 15:40:30 -0700

    - Fixed Utilities function that incorrectly interpreted strings on windows machines - Updated Jar

--- src/Engine/Utilities.java
+++ src/Engine/Utilities.java
@@ -1,29 +1,20 @@
 package Engine;
 
-
 final public class Utilities {
 	
 	public static int[][] StringToIntArray(String str) {
-		int numTiles = str.length();
-		final int COLS = str.indexOf(System.lineSeparator());
-		final int ROWS = numTiles / COLS;
+        String[] data = str.split(System.lineSeparator());
+        
+		final int ROWS = data.length;
+		final int COLS = data[0].length();
+		
 		int[][] ar = new int[ROWS][COLS];
 		
 		char tilechar;
-		int strindex = 0;
-		
 		for (int row = 0; row < ROWS; row++) {
 			for (int col = 0; col < COLS; col++) {
-				
-				if (strindex < str.length()) {
-					do {
-						tilechar = str.charAt(strindex);
-						strindex++;
-					} while(!Character.isDigit(tilechar) && strindex < str.length());
-					
-					ar[row][col] = Character.getNumericValue(tilechar);
-				}
-				
+				tilechar = data[row].charAt(col);
+				ar[row][col] = Character.getNumericValue(tilechar);
 			}
 		}
 		
